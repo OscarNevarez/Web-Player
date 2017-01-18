b6566cb303dea65daeae8511e55688e59ca0d19a
Exia <onevarez@cpp.edu>
2017-01-17 17:39:33 -0800

    added code for streaming a mp4 ran into some problems

--- app.js
+++ app.js
@@ -3,8 +3,12 @@
  * Module dependencies.
  */
 
-var express = require('express')
-var app = express()
+var express = require("express");
+const url = require("url");
+var fs = require("fs");
+var http = require("http");
+var path = require("path");
+var app = express();
 
 app.get('/watch', function (req, res) {
 	var options = { 
@@ -14,6 +18,34 @@ app.get('/watch', function (req, res) {
 	res.sendFile('home.html', options);
 })
 
+app.get('/s4', function(req, res){
+	var file = path.join(__dirname, "/assets/scream4.mp4");
+	console.log(__dirname);
+	var range = req.headers.range;
+	if(!range) {
+		return res.sendStatus(416);
+	}
+	var positions = range.replace(/bytes=/, "").split("-");
+	var start = parseInt(positions[0], 10);
+	var total = stats.size;
+	var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+	var chunksize = (end - start) + 1 
+	
+	res.writeHead(206, {
+		"Content-Range": "bytes " + start + "-" + end + "/" + total,
+		"Accept-Ranges": "bytes",
+		"Content-Length": chunksize,
+		"Content-Type": "video/mp4"
+	})
+	
+	var stream =fs.createReadStream(file, { start: start, end: end })
+		.on("open", function(){
+			stream.pipe(res);
+		}).on("error", function(err){
+			res.end(err);
+		});
+})
+
 app.listen(3000, function () {
-	console.log("Server is running!")
+	console.log("Server is running!");
 })
