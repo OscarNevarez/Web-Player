c9e0c67179ddd4b1e3eaf180f826b6902a63a3ab
Exia <onevarez@cpp.edu>
2017-01-17 18:42:16 -0800

    maybe done?

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var fs = require("fs");
 var http = require("http");
 var path = require("path");
 var app = express();
+app.use(express.static('assets'));
 
 app.get('/watch', function (req, res) {
 	var options = { 
@@ -16,34 +17,6 @@ app.get('/watch', function (req, res) {
 	}
 	res.set('Content-Type', 'text/html');
 	res.sendFile('home.html', options);
-})
-
-app.get('/s4', function(req, res){
-	var file = path.join(__dirname, "/assets/scream4.mp4");
-	console.log(__dirname);
-	var range = req.headers.range;
-	if(!range) {
-		return res.sendStatus(416);
-	}
-	var positions = range.replace(/bytes=/, "").split("-");
-	var start = parseInt(positions[0], 10);
-	var total = stats.size;
-	var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-	var chunksize = (end - start) + 1 
-	
-	res.writeHead(206, {
-		"Content-Range": "bytes " + start + "-" + end + "/" + total,
-		"Accept-Ranges": "bytes",
-		"Content-Length": chunksize,
-		"Content-Type": "video/mp4"
-	})
-	
-	var stream =fs.createReadStream(file, { start: start, end: end })
-		.on("open", function(){
-			stream.pipe(res);
-		}).on("error", function(err){
-			res.end(err);
-		});
 })
 
 app.listen(3000, function () {
